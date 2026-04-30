8b0c592d9b8f57ddef96749755f004cff47bce86
Erwin Saul <mackonkey_2@hotmail.com>
2026-04-30 09:59:55 -0400

    feat(excel__exporter): implement ExcelExporter by extracting logic from web_app

---
+++ utils/excel_exporter.py
@@ -0,0 +1,13 @@
+"""Exportador de reportes a Excel"""
+
+import openpyxl
+from openpyxl.styles import Font, PatternFill, Alignment
+import os
+from datetime import datetime
+
+class ExcelExporter:
+    @staticmethod
+    def generar_reporte_paralelo(paralelo, ruta_archivo=None):
+        """ Genera un reporte a Excel """
+        NotImplementedError("Implementacion pendiente")
+        
